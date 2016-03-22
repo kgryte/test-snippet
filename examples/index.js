0b2bcd52322cb64ca3dec2130d6c7632805ca200
Planeshifter <pburckhardt@outlook.com>
2016-03-22 11:16:14 -0400

    Update examples

--- examples/index.js
+++ examples/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var mkdirp = require( 'mkdirp' ),
-	path = require( 'path' ),
-	cp = require( './../lib' );
+var mkdirp = require( 'mkdirp' );
+var path = require( 'path' );
+var cp = require( './../lib' );
 
 var dirpath = path.resolve( __dirname, '../build/' + new Date().getTime() );
 
 mkdirp.sync( dirpath );
 cp.sync( dirpath, {
 	'template': 'mocha',
-	'title': 'beep-boop'
+	'title': 'beep-boop',
+	'filename': 'test.beep.js'
 });
-
