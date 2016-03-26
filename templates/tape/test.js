d8561ab91fce689f9f9e24c1d9e487d04bae70f7
kgryte <kgryte@gmail.com>
2016-03-25 23:19:41 -0700

    Update tape snippet to print the test filename

--- templates/tape/test.js
+++ templates/tape/test.js
@@ -9,6 +9,7 @@ var lib = require( './../lib' );
 // TESTS //
 
 tape( 'main export is a function', function test( t ) {
+	t.ok( true, __filename );
 	t.equal( typeof lib, 'function', 'main export is a function' );
 	t.end();
 });
